982890dc588ecf8e78adc2cc9e447d8519b4f392
rabiulsarker <rabiul.sarker@polygontech.xyz>
2024-02-18 15:20:08 +0600

    project_setup

---
+++ src/main/java/com/example/demo_blog_automation/Login.java
@@ -0,0 +1,13 @@
+package com.example.demo_blog_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class Login 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/example/demo_blog_automation/LoginTest.java
@@ -0,0 +1,21 @@
+package com.example.demo_blog_automation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LoginTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println();
+    }
+}
